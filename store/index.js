9830ed2c6ae57f4fbeefd58e02f27dbbc8468fed
Benedicte Raae <raae@bgraphic.no>
2017-07-05 13:03:44 +0200

    Upload image to store

--- store/index.js
+++ store/index.js
@@ -1,7 +1,31 @@
 export const state = () => ({
-
+  image: {
+    dataUrl: null,
+    status: null
+  }
 });
 
 export const mutations = {
+  imageStatus(state, status) {
+    state.image.status = status;
+  },
+  imageDataUrl(state, dataUrl) {
+    console.log('mutations imageDataUrl');
+    state.image.dataUrl = dataUrl;
+  }
 
+};
+
+export const actions = {
+  uploadFile({ commit }, file) {
+    const fReader = new FileReader();
+    commit('imageStatus', 'IN_PROGRESS');
+
+    fReader.onload = () => {
+      console.log('actions onLoad');
+      commit('imageDataUrl', fReader.result);
+    };
+
+    fReader.readAsDataURL(file);
+  }
 };
