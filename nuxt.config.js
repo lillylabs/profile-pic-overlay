8dd01c022c774df375f8c1ff1dec44400adcd5e5
Benedicte Raae <raae@bgraphic.no>
2017-07-25 11:36:20 +0200

    Favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,14 +7,16 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: process.env.TITLE || '#HunSpanderer',
+    title: process.env.TITLE || 'HunSpanderer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.DESCRIPTION || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'apple-touch-icon-precomposed', sizes: '152x152', href: '/apple-icon-152x152.png' },
+      { rel: 'icon', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', sizes: '16x16', href: '/favicon-16x16.png' },
     ]
   },
   /*
