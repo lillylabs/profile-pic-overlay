208de8fb4638266caa1e60354a60e68b90b55a7f
Benedicte Raae <raae@bgraphic.no>
2017-10-03 14:04:35 +0200

    Checkin production

--- plugins/ga.js
+++ plugins/ga.js
@@ -2,18 +2,20 @@
 
 export default ({ app: { router } }) => {
 
-  if (!process.env.NODE_ENV === 'production') {
+  console.log(process.env.NODE_ENV);
+
+  if (process.env.NODE_ENV !== 'production') {
     return;
   }
 
-  console.log('Init GA', process.env.GA_CODE)
+  console.log('Init GA', process.env.GA_CODE);
 
-    (function (i, s, o, g, r, a, m) {
-      i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
-        (i[r].q = i[r].q || []).push(arguments)
-      }, i[r].l = 1 * new Date(); a = s.createElement(o),
-        m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+  (function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+      (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date(); a = s.createElement(o),
+      m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+  })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
   // Every time the route changes (fired on initialization too)
   router.afterEach((to, from) => {
@@ -22,5 +24,5 @@ export default ({ app: { router } }) => {
   });
 
   // Set the current page
-  ga('create', process.env.GA_CODE, 'auto')
+  ga('create', process.env.GA_CODE, 'auto');
 }
